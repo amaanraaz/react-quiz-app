598488deeabb26448077686f736f2281c320d9d8
amaanraaj <amandwivedi00@gmail.com>
2022-11-23 17:05:25 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import questions from './data';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	const [currentIndex, setCurrentIndex] = useState(0)
+	function handleAnswerClick(isCorrect) {
+        if(isCorrect){
+            setScore(score+1);
+        }        
+
+		if (currentIndex === questions.length - 1) {
+			// quiz over
+			setQuizFinished(true)
+		} else {
+			setCurrentIndex((value) => value + 1)
+		}
+
+	}
+
+const [quizFinished, setQuizFinished] = useState(false)	
+    const [score,setScore] = useState(0)
+
+	return (
+		<div className="app">
+			{quizFinished ? (
+				<div className="score-section">
+					You scored {score} out of {questions.length}
+				</div>
+			) : (
+				<>
+					<div className="question-section">
+						<div className="question-count">
+							<span>Question 1</span>/{questions.length}
+						</div>
+						<div className="question-text">
+							{questions[currentIndex].questionText}
+						</div>
+					</div>
+					<div className="answer-section">
+						{questions[currentIndex].answerOptions.map((answer) => {
+							return (
+								<button
+									onClick={()=>handleAnswerClick(answer.isCorrect)}
+									key={answer.answerText}
+								>
+									{answer.answerText}
+								</button>
+							)
+						})}
+					</div>
+				</>
+			)}
+		</div>
+	)
 }
-
-export default App;
---
+++ src/data.js
@@ -0,0 +1,76 @@
+const questions = [
+    {
+        questionText: 'Which of the following command is used to create react-js-app ?',
+        answerOptions: [
+            { answerText: 'npx create-react-app appname', isCorrect: true },
+            { answerText: 'npm install create-react-app', isCorrect: false },
+            { answerText: 'npx install create-react-app -g', isCorrect: false },
+            { answerText: 'install - l create-react-app', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'In React.js which one of the following is used to create a class for Inheritance ?',
+        answerOptions: [
+            { answerText: 'Create', isCorrect: false },
+            { answerText: 'Extends', isCorrect: true },
+            { answerText: 'Inherits', isCorrect: false },
+            { answerText: 'Delete', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Which of the following valid component return type of React ?',
+        answerOptions: [
+            { answerText: '1', isCorrect: true },
+            { answerText: '2', isCorrect: false },
+            { answerText: '5', isCorrect: false },
+            { answerText: '3', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'What is the default port number in which the application run ?',
+        answerOptions: [
+            { answerText: '8080', isCorrect: false },
+            { answerText: '5000', isCorrect: false },
+            { answerText: '3030', isCorrect: false },
+            { answerText: '3000', isCorrect: true },
+        ],
+    },
+{
+        questionText: 'Which of the following is a way to handle data in React.js ?',
+        answerOptions: [
+            { answerText: 'Services & Components', isCorrect: false },
+            { answerText: 'State & Services', isCorrect: false },
+            { answerText: 'State & Component', isCorrect: false },
+            { answerText: 'State & Props', isCorrect: true },
+        ],
+    },
+{
+        questionText: 'Which of the following is must for the API in React.js ?',
+        answerOptions: [
+            { answerText: 'SetinitialComponent', isCorrect: false },
+            { answerText: 'render', isCorrect: false },
+            { answerText: 'renderComponent', isCorrect: true },
+    { answerText: 'All of these', isCorrect: false },
+        ],
+    },
+{
+        questionText: 'Which of the following is true regarding Babel ?',
+        answerOptions: [
+            { answerText: 'Compiler', isCorrect: false },
+            { answerText: 'Transpilar', isCorrect: false },
+            { answerText: 'Both', isCorrect: false },
+            { answerText: 'None', isCorrect: true },
+        ],
+    },
+{
+        questionText: 'In React.js, how we can pass the data from one component to another in React.js ?',
+        answerOptions: [
+            { answerText: 'SetState', isCorrect: false },
+            { answerText: 'Render with arguments', isCorrect: false },
+            { answerText: 'Props', isCorrect: true },
+            { answerText: 'PropTypes', isCorrect: false },
+        ],
+    },
+]
+
+export default questions;
